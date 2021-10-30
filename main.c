81ac141ed1430ba0f25b8be8fa70993383ac9ea6
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-10-31 01:20:57 +0200

    Add rules to gitignore and change hello world text to all star

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	printf("Hello, World !");
+	printf("Somebody once told me the world is gonna roll me\nI ain't the sharpest tool in the shed\nShe was looking kind of dumb with her finger and her thumb\nIn the shape of an \"L\" on her forehead\n");
 	return 0;
 }
